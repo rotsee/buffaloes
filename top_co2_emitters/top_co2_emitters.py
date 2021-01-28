91ff61c8a39861f98beeb12955c390ca305d5932
Papanash <Papanash@datastory.org>
2021-01-28 11:45:14 +0100

    pip stuff

--- top_co2_emitters/top_co2_emitters.py
+++ top_co2_emitters/top_co2_emitters.py
@@ -7,6 +7,18 @@ import dash_html_components as html
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-from app import app
 
-tls.get_embed('https://chart-studio.plotly.com/~papanash/9')
+#from app import app
+
+
+def html_title(title):
+    return html.Div([html.H1(title)],
+                    style={'textAlign': 'center'})
+
+layout = html.Div(children=[
+
+        html_title('Top co2 emitters'),
+        html.Br(), html.Br(),
+
+        html.Embed(src='https://chart-studio.plotly.com/~papanash/9.embed', height="1200", width="100%")
+])
