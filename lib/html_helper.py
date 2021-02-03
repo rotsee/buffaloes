94a20be019d5f3fea04308a32a647989af907ea1
Papanash <Papanash@datastory.org>
2021-02-03 11:33:06 +0100

    add helper

---
+++ lib/html_helper.py
@@ -0,0 +1,28 @@
+import os
+from pathlib import Path
+
+import dash_core_components as dcc
+import dash_html_components as html
+
+
+def get_project_root() -> Path:
+    return Path(__file__).parent.parent
+
+def html_title(title):
+    return html.Div([html.H1(title)],
+                    style={'textAlign': 'center'})
+
+def html_body(story, items:list):
+    intro_file = Path.joinpath(get_project_root(), story, 'intro.md')
+    if os.path.exists(intro_file):
+        intro_text = """{}""".format(open(intro_file).read())
+        items = [dcc.Markdown(intro_text)] + items
+
+    conclusion_file = Path.joinpath(get_project_root(), story, 'conclusion.md')
+    if os.path.exists(conclusion_file):
+        conclusion_text = """{}""".format(open(conclusion_file).read())
+        items = items + [dcc.Markdown(conclusion_text)]
+
+    items = [v for i in items for v in (html.Br(),html.Br(), i)]
+
+    return html.Div(items)
--- top_co2_emitters/top_co2_emitters.py
+++ top_co2_emitters/top_co2_emitters.py
@@ -7,18 +7,10 @@ import dash_html_components as html
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-
-#from app import app
-
-
-def html_title(title):
-    return html.Div([html.H1(title)],
-                    style={'textAlign': 'center'})
+from lib.html_helper import *
 
 layout = html.Div(children=[
-
-        html_title('Top co2 emitters'),
-        html.Br(), html.Br(),
-
-        html.Embed(src='https://chart-studio.plotly.com/~papanash/9.embed', height="1200", width="100%")
+    html_title('Top co2 emitters'),
+    html_body('top_co2_emitters',
+              [html.Embed(src='https://chart-studio.plotly.com/~papanash/9.embed', height="1000", width="100%")])
 ])
