acb6d22c1d5f45d0763761da341a6abbe3c9006d
Papanash <Papanash@datastory.org>
2021-02-16 18:01:14 +0100

    refactor

--- climate_goals/climate_goals.py
+++ climate_goals/climate_goals.py
@@ -20,6 +20,7 @@ if len(files) == 1:
     csv_file = files[0]
 
 df_pct_change = pd.read_csv(csv_file, index_col=0)
+emission_color = px.colors.qualitative.Plotly[0]
 
 
 def project(df, target_year, current_year, current_value, change):
@@ -46,15 +47,16 @@ def climate_goal_projection(measure='value', target=2045, target_value_max=10.7)
     df = df_pct_change
     fig = go.Figure()
 
-    fig.add_trace(go.Bar(x=df.year, y=df[measure], marker_color='light blue'))
     fig.add_trace(
-        go.Bar(x=[target], y=[target_value_max], marker_color='green'))
+        go.Bar(x=df.year, y=df[measure], marker_color=emission_color))
     fig.update_traces(showlegend=False)
+    fig.add_trace(
+        go.Bar(x=[target], y=[target_value_max], marker_color='green', name='emission offset'))
 
     last = df.iloc[-1]
 
     fig.add_trace(go.Scatter(x=[last.year, target], y=[last.value, 38.3],
-                             name='1.1% reduction (current average)', line=dict(color='light blue', dash='dash')))
+                             name='1.1% reduction (current average)', line=dict(color=emission_color, dash='dash')))
     fig.add_trace(go.Scatter(x=[last.year, target], y=[
         last.value, 0], name='6% reduction (offset needed)', line=dict(color='green', dash='dot')))
     fig.add_trace(go.Scatter(x=[last.year, target], y=[
@@ -76,9 +78,10 @@ def transport_goal_projection(measure='domestic_transport', target=2030, target_
     df = df_pct_change[df_pct_change.year > 2009]
     fig = go.Figure()
 
-    fig.add_trace(go.Bar(x=df.year, y=df[measure], marker_color='light blue'))
     fig.add_trace(
-        go.Bar(x=[target], y=[target_value_max], marker_color='light blue'))
+        go.Bar(x=df.year, y=df[measure], marker_color=emission_color))
+    fig.add_trace(
+        go.Bar(x=[target], y=[target_value_max], marker_color=emission_color))
     fig.update_traces(showlegend=False)
 
     last = df.iloc[-1]
@@ -88,7 +91,7 @@ def transport_goal_projection(measure='domestic_transport', target=2030, target_
     project_pct = abs(round(df['pct_change_domestic_transport'].mean(), 1))
 
     fig.add_trace(go.Scatter(x=[last.year, target], y=[last[measure], 14.5],
-                             name=str(project_pct) + '% reduction (current average)', line=dict(color='light blue', dash='dash')))
+                             name=str(project_pct) + '% reduction (current average)', line=dict(color=emission_color, dash='dash')))
 
     tick_years = [2010, 2020, 2030]
     fig.update_layout(yaxis={'title': 'Emissions (M tons)'},
